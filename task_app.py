1e9d40e1e847e0f36dec1f975171039ad7f742bb
saraybu <orelgerda12@gmail.com>
2022-04-29 16:08:22 +0300

    add sqlite and streamlit

--- task_app.py
+++ task_app.py
@@ -4,4 +4,5 @@ import streamlit as st
 from src.main_deploy import MainDeploy
 
 urls_df = MainDeploy(is_streamlit_deploy=True).run(True)
-st.write(pd.DataFrame(urls_df[0]))
+for df in urls_df:
+    st.write(df)
