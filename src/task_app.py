88f012b8eab65053c022b9ea2a35e083215812b0
saraybu <orelgerda12@gmail.com>
2022-04-29 15:25:39 +0300

    add sqlite and streamlit

--- src/task_app.py
+++ src/task_app.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import streamlit as st
 
-from src.database.sqlite_database import SqlLiteDataBase
+# from src.database.sqlite_database import SqlLiteDataBase
 from src.main_config import DB_NAME
 
-sql_lite = SqlLiteDataBase()
-sql_lite.create_db(DB_NAME)
+# sql_lite = SqlLiteDataBase()
+# sql_lite.create_db(DB_NAME)
 # MainDeploy().run()
 # urls_df = pd.read_sql(URLS_CLASSIFICATION_TABLE, sql_lite.connection)
 st.write(pd.DataFrame([1, 2, 3]))
