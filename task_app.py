e5789f4dd392c8454c35614cfade6a82a47a5e55
saraybu <orelgerda12@gmail.com>
2022-05-01 03:04:10 +0300

    fix bugs

--- task_app.py
+++ task_app.py
@@ -4,7 +4,7 @@ from src.database.sqlite_database import SqlLiteDataBase
 from src.main_config import DB_NAME, URLS_CLASSIFICATION_TABLE, CATEGORIES_TABLE, VOTING_TABLE
 from src.main_deploy import MainDeploy
 
-# task_dfs = MainDeploy(is_streamlit_deploy=True).run(return_df=True)
+MainDeploy(is_streamlit_deploy=True).run(return_df=False)
 task_dfs= []
 sql_lite = SqlLiteDataBase().connect_db(DB_NAME)
 task_dfs.append(pd.read_sql(f'''select * from {URLS_CLASSIFICATION_TABLE}''',sql_lite.connection))
