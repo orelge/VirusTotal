09a8c4c8b2cb5098aa628c0d65a247f91cf6bacf
saraybu <orelgerda12@gmail.com>
2022-05-01 03:10:31 +0300

    fix bugs

--- task_app.py
+++ task_app.py
@@ -5,7 +5,7 @@ from src.main_config import DB_NAME, URLS_CLASSIFICATION_TABLE, CATEGORIES_TABLE
 from src.main_deploy import MainDeploy
 
 
-task_dfs = MainDeploy(is_streamlit_deploy=True).run(return_df=False)
+task_dfs = MainDeploy(is_streamlit_deploy=True).run(return_df=True)
 # task_dfs.append(pd.read_sql(f'''select * from {URLS_CLASSIFICATION_TABLE}''',sql_lite.connection))
 # task_dfs.append(pd.read_sql(f'''select * from {CATEGORIES_TABLE}''',sql_lite.connection))
 # task_dfs.append(pd.read_sql(f'''select * from {VOTING_TABLE}''',sql_lite.connection))
