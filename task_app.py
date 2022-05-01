231e7c072714eb4b3ce40b079f348161bb197386
saraybu <orelgerda12@gmail.com>
2022-05-01 03:06:10 +0300

    fix bugs

--- task_app.py
+++ task_app.py
@@ -4,12 +4,13 @@ from src.database.sqlite_database import SqlLiteDataBase
 from src.main_config import DB_NAME, URLS_CLASSIFICATION_TABLE, CATEGORIES_TABLE, VOTING_TABLE
 from src.main_deploy import MainDeploy
 
-MainDeploy(is_streamlit_deploy=True).run(return_df=False)
-task_dfs= []
+
+task_dfs = MainDeploy(is_streamlit_deploy=True).run(return_df=False)
+
 sql_lite = SqlLiteDataBase().connect_db(DB_NAME)
-task_dfs.append(pd.read_sql(f'''select * from {URLS_CLASSIFICATION_TABLE}''',sql_lite.connection))
-task_dfs.append(pd.read_sql(f'''select * from {CATEGORIES_TABLE}''',sql_lite.connection))
-task_dfs.append(pd.read_sql(f'''select * from {VOTING_TABLE}''',sql_lite.connection))
+# task_dfs.append(pd.read_sql(f'''select * from {URLS_CLASSIFICATION_TABLE}''',sql_lite.connection))
+# task_dfs.append(pd.read_sql(f'''select * from {CATEGORIES_TABLE}''',sql_lite.connection))
+# task_dfs.append(pd.read_sql(f'''select * from {VOTING_TABLE}''',sql_lite.connection))
 
 for df, title in zip(task_dfs, [URLS_CLASSIFICATION_TABLE, CATEGORIES_TABLE, VOTING_TABLE]):
     st.title(title)
