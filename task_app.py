e6d3ec314e8c0b4daa0b134707ad26ceefadf4e9
saraybu <orelgerda12@gmail.com>
2022-05-01 03:23:30 +0300

    Revert "fix bugs"
    This reverts commit 3bf2a7b7

--- task_app.py
+++ task_app.py
@@ -7,9 +7,9 @@ from src.main_deploy import MainDeploy
 
 task_dfs = MainDeploy(is_streamlit_deploy=True).run(return_df=True)
 sql_lite = SqlLiteDataBase().connect_db(DB_NAME)
-task_dfs.append(pd.read_sql(f'''select * from {URLS_CLASSIFICATION_TABLE}''',sql_lite.connection))
-task_dfs.append(pd.read_sql(f'''select * from {CATEGORIES_TABLE}''',sql_lite.connection))
-task_dfs.append(pd.read_sql(f'''select * from {VOTING_TABLE}''',sql_lite.connection))
+# task_dfs.append(pd.read_sql(f'''select * from {URLS_CLASSIFICATION_TABLE}''',sql_lite.connection))
+# task_dfs.append(pd.read_sql(f'''select * from {CATEGORIES_TABLE}''',sql_lite.connection))
+# task_dfs.append(pd.read_sql(f'''select * from {VOTING_TABLE}''',sql_lite.connection))
 
 for df, title in zip(task_dfs, [URLS_CLASSIFICATION_TABLE, CATEGORIES_TABLE, VOTING_TABLE]):
     st.title(title)
